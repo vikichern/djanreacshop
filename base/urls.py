1e044ca3df7ac90df6da815b355502bf31c8249c
Your Name <victoriac555@gmail.com>
2022-12-26 22:29:37 +0200

    first commit

---
+++ base/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from. import views
+
+urlpatterns = [
+    path('', views.index),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('products', views.myProducts),
+    path('test', views.test),
+    path('pub', views.ppub),
+]
---
+++ base/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from .models import Product
+from .serializer import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+ 
+def index(req):
+    return JsonResponse('hello', safe=False)
+
+def ppub(req):
+    return JsonResponse('no auth required', safe=False)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def myProducts(req):
+    all_products = ProductSerializer(Product.objects.all(), many=True).data
+    return JsonResponse(all_products, safe=False)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def test(req):
+    return JsonResponse({"authentication test":"ok"}, safe=False)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+ 
+        # Add custom claims
+        token['username'] = user.username
+        token['emaillll'] = user.email
+        # ...
+ 
+        return token
+ 
+ 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
